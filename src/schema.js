a56ac583b27d5bee2486822aa10daa07b820c7f3
elaheabs <elahe.abs@gmail.com>
2022-07-14 16:00:41 -0300

    deprecated a feild

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,10 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated
+    "We will return this name for the length to the client from now on to prevent mislead "
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
